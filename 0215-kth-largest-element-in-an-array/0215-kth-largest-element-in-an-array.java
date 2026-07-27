cb29455a81207134498b4b2b189fd1b8ddebbecd
Shaurya <148006944+Shauryakamal@users.noreply.github.com>
2026-07-27 22:45:46 +0530

    Time: 99 ms (12.53%), Space: 72.7 MB (84.4%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int findKthLargest(int[] arr, int k) {
+        mergeSort(arr,0,arr.length-1);
+        return arr[arr.length-k];
+    }
+   public static void mergeSort(int[] arr, int low, int high) {
+        if (low == high) {
+            return;
+        }
+
+        int mid = low + (high - low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+     public static void merge(int[] arr, int low, int mid, int high) {
+        int left = low;
+        int right = mid + 1;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+}
